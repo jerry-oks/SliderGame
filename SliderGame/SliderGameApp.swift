fbfea55c2fd5a7a68d96ed9a1bde5692b14f1ec4
Vadim Vanatov <79990006611@yandex.ru>
2023-10-23 21:06:22 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by HOLY NADRUGANTIX on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
