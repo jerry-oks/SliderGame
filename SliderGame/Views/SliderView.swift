ce97959789e422f8fae97d1641636b6e3d415891
Vadim Vanatov <79990006611@yandex.ru>
2023-10-25 11:01:17 +0300

    environment object + slider view + cheat mode label changes + computing changes

---
+++ SliderGame/Views/SliderView.swift
@@ -0,0 +1,18 @@
+//
+//  SliderView.swift
+//  SliderGame
+//
+//  Created by HOLY NADRUGANTIX on 25.10.2023.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderView()
+}
